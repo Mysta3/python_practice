17d04269356491ab7ccb36cd55383a38508704ca
Mysta3 <myshawnestallings@gmail.com>
2020-12-31 08:17:03 -0800

    first commit

---
+++ practice.py
@@ -0,0 +1,14 @@
+print("What is your name")
+name = input()
+# print(name)
+# reassign = name
+
+# if name == 'hello':
+#     print('Word')
+# else:
+#     print('You are ugly!')
+
+# if name.lower() == 'hello':
+#     print('YOU GOT IT RIGHT')
+# else:
+#     print('ya motha')
